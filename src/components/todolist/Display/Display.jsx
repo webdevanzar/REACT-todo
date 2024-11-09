2274af8ec79cbd10c9211548d9a821f795e10a10
Muhammed anzarshah <anzarsha3240@gmail.com>
2024-11-09 09:48:01 +0530

    third commit

--- src/components/todolist/Display/Display.jsx
+++ src/components/todolist/Display/Display.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Display.css";
 
-export const Display = ({ dataItems, deleteItems ,editItem}) => {
+export const Display = ({ dataItems, deleteItems, editItem }) => {
+  const [completedItems, setCompletedItems] = useState([]);
 
-
+  const completed = (id) => {
+    setCompletedItems((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+  console.log(completedItems);
+  
   return (
     <div className="display-main-container">
       {dataItems.map((data) => {
+        const isCompleted = completedItems[data.id] || false;
+
         return (
           <div className="display-container" key={data.id}>
-            <p> {data.text} </p>
-            <div className="button-container" key={data.id} >
+            <p
+              onClick={() => completed(data.id)}
+              style={{ textDecoration: isCompleted ? "line-through" : "none" }}
+            >
+              {data.text}
+            </p>
+            <div className="button-container" key={data.id}>
               <img
                 src={"/src/images/edit.png"}
                 alt=""
